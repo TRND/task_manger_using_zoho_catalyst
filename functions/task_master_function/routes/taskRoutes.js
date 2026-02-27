59a922dd19eddeaaae0a297f7443ad8d17265035
Joe Achakji <101284884+joeachakji@users.noreply.github.com>
2026-02-27 12:54:25 -0500

    Update taskRoutes.js

--- functions/task_master_function/routes/taskRoutes.js
+++ functions/task_master_function/routes/taskRoutes.js
@@ -4,9 +4,14 @@ const { taskValidation } = require('../middlewares/validation');
 
 const router = express.Router();
 
-router.get('/:userId', getTasks);
+// No userId in the URL anymore.
+// Backend will use the logged in user from Catalyst.
+router.get('/', getTasks);
+
 router.post('/', taskValidation, createTask);
+
 router.put('/:id', taskValidation, updateTask);
+
 router.delete('/:id', deleteTask);
 
 module.exports = router;
