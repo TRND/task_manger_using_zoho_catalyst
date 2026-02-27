52d99357829bab9d4432cd36202448142edc6dfd
Joe Achakji <101284884+joeachakji@users.noreply.github.com>
2026-02-27 16:32:32 -0500

    Update App.tsx

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import TaskList from './components/TaskList';
@@ -6,24 +5,14 @@ import LoginForm from './components/LoginForm';
 import SignupForm from './components/SignupForm';
 
 const App = () => {
-  const isLoggedIn = useSelector((state:any) => !!state.user.userInfoTh);
+  const isLoggedIn = useSelector((state: any) => !!state.user.userInfoTh);
 
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/app" />} />
-      <Route 
-        path="/app" 
-        element={!isLoggedIn ? <LoginForm /> : <Navigate to="/app/dashboard" />} 
-      />
-      <Route 
-        path="/app/signup" 
-        element={!isLoggedIn ? <SignupForm /> : <Navigate to="/app/dashboard" />} 
-      />
-      <Route 
-        path="/app/dashboard" 
-        element={isLoggedIn ? <TaskList /> : <Navigate to="/app/" />} 
-      />
-      <Route path="*" element={<Navigate to="/app" />} />
+      <Route path="/" element={!isLoggedIn ? <LoginForm /> : <Navigate to="/dashboard" />} />
+      <Route path="/signup" element={!isLoggedIn ? <SignupForm /> : <Navigate to="/dashboard" />} />
+      <Route path="/dashboard" element={isLoggedIn ? <TaskList /> : <Navigate to="/" />} />
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 };
